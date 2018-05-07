6109f72ce4c752a7873a19adf7d3cd4875f88226
Brian Mendoza <brian.mendoza@deciphernow.com>
2018-05-07 07:39:52 -0400

    flatMap util

--- _utils.js
+++ _utils.js
@@ -1,7 +1,7 @@
 /**
  * Simple function to log a value
  * and return the value logged
- * @param {any} value
+ * @param any value
  * @returns
  */
 const log = value => {
@@ -13,9 +13,9 @@ const log = value => {
  * Call a function n times with the same args,
  * returning an array of returned values
  * e.g. repeat(fn)(5)(arg1, arg2, arg3)
- * @param {function} fn. What function to call
- * @param {number} n. How many times to repeat
- * @param {any} ...args. Optional arguments
+ * @param function fn. What function to call
+ * @param number n. How many times to repeat
+ * @param any ...args. Optional arguments
  * @returns [any] Values returned from each function call
  */
 const repeat = fn => n =>
@@ -27,8 +27,8 @@ const repeat = fn => n =>
  * Call a function with a new arg/args each call,
  * returning an array of returned values
  * e.g. map(fn)(arg1, arg2, null, [arg3a, arg3b])
- * @param {function} fn. What function to call
- * @param {any} ...args. Arguments for each
+ * @param function fn. What function to call
+ * @param any ...args. Arguments for each
  * @returns [any] Values returned from each function call
  */
 const map = fn => (...args) =>
@@ -39,25 +39,19 @@ const map = fn => (...args) =>
 
 /**
  * Call a function with a new arg/args each call,
- * then reduce their returned values into one value
- * via a given reducer function
- * e.g. reduce(fn)((a, b) => a + b)(arg1, arg2, null, [arg3a, arg3b])
- * @param {function} fn. What function to call
- * @param {function} reducer. The reducer
- * @param {any} ...args. Arguments for each
+ * then flatten an array of their returned values
+ * into one value via a given reducer function
+ * e.g. flatMap(fn)((a, b) => a + b)(arg1, arg2, null, [arg3a, arg3b])
+ * @param function fn. What function to call
+ * @param function reducer. The reducer
+ * @param any ...args. Arguments for each
  * @returns [any] Values returned from each function call
  */
-const reduce = fn => reducer => (...args) =>
-  args
-    .map(el => {
-      if (Array.isArray(el)) return fn(...el);
-      else return fn(el);
-    })
-    .reduce(reducer);
+const flatMap = fn => reducer => (...args) => map(fn)(...args).reduce(reducer);
 
 module.exports = {
   log,
   repeat,
   map,
-  reduce
+  flatMap
 };
